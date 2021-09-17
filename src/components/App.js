b1d4dce9ee9ec20ab3056ee334a2ecdc4490ffb0
liux <liu-xin@zhimingsoft.cn>
2021-09-17 17:01:11 +0800

    simple netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> DGW DV
+        <strong>{results.length}</strong> liux :-)
       </p>
       <hr></hr>
       {results
